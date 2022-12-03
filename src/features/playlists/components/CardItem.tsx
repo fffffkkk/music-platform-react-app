04d3e4a9e74d822ef8df31684ee101132f969117
fffffkkk <alexuppereks@gmail.com>
2022-12-04 01:59:49 +0300

    add playlists component

---
+++ src/features/playlists/components/CardItem.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+
+import {useNavigate} from "react-router-dom";
+
+import {IPlaylist} from "@/models/IPlaylist";
+
+interface CardItemProps {
+	data: IPlaylist;
+};
+
+const CardItem: FC<CardItemProps> = ({data}) => {
+	const navigate = useNavigate();
+	const {title, id} = data;
+	
+	return (
+		<div className="card w-96 bg-slate-500 shadow-xl cursor-pointer" onClick={() => navigate(`/playlist/${id}`)}>
+			<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes"/></figure>
+			<div className="card-body">
+				<h2 className="card-title">
+					{title}
+				</h2>
+			</div>
+		</div>
+	);
+};
+
+export default CardItem;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,17 +2,18 @@ import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {setupListeners} from "@reduxjs/toolkit/query";
 
 import {userReducer} from "@/store/user/user.slice";
+import {playlistAPI} from "@/services/playlistService";
 
 export const rootReducer = combineReducers({
+	[playlistAPI.reducerPath]: playlistAPI.reducer,
 	user: userReducer,
 })
 
 export const store = configureStore({
 	reducer: rootReducer,
-	// middleware: (getDefaultMiddleware) => {
-	// 	getDefaultMiddleware().concat()
-	// }
-})
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(playlistAPI.middleware),
+});
 
 setupListeners(store.dispatch)
 
